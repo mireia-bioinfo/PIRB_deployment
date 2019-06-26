6573735bd100f6008c8956fef5740c26573b58bb
Mireia Ramos <mireiarr9@gmail.com>
2019-06-26 20:32:44 +0200

    Update to R-3.5.1 and bioc3.8

--- regulomedeps/install_regulomedeps.R
+++ regulomedeps/install_regulomedeps.R
@@ -1,6 +1,6 @@
 # template taken from bioconductor
 # https://github.com/Bioconductor/bioc_docker/blob/master/out/release_core/install.R
-library(BiocInstaller)
+library(BiocManager)
 
 #
 cores <- parallel::detectCores()
@@ -35,6 +35,5 @@ pkglist <- c(
              "shinyjs"
              )
 
-
-biocLite(pkglist)
+install(pkglist)
 
